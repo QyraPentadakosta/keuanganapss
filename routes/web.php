becf0d4f80e56386c2e991e8328e4e44d3250a61
Qyra98 <74602996+Qyra98@users.noreply.github.com>
2022-02-07 11:33:02 +0700

    md

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Auth;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,3 +17,16 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+
+Route::group(['prefix' => 'admin', 'middleware' => ['auth', 'auth.admin']], function(){
+    Route::get('/', function () {
+        return 'a';
+    });
+});
+
+
